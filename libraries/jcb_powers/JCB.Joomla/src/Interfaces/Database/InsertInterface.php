93a6efabf96204d8c12bdf3f850bb6675ce6f24a
gacompa <gacompa@gmail.com>
2025-07-31 05:53:01 +0200

    Add files via upload

---
+++ libraries/jcb_powers/JCB.Joomla/src/Interfaces/Database/InsertInterface.php
@@ -0,0 +1,78 @@
+<?php
+
+/***[JCBGUI.power.licensing_template.134.$$$$]***/
+/**
+ * @package    Joomla.Component.Builder
+ *
+ * @created    4th September, 2022
+ * @author     Llewellyn van der Merwe <https://dev.vdm.io>
+ * @git        Joomla Component Builder <https://git.vdm.dev/joomla/Component-Builder>
+ * @copyright  Copyright (C) 2015 Vast Development Method. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+/***[/JCBGUI$$$$]***/
+
+namespace JCB\Joomla\Interfaces\Database;
+
+
+use JCB\Joomla\Interfaces\Database\DefaultInterface;
+use JCB\Joomla\Interfaces\Database\VersioningInterface;
+
+
+/**
+ * Database Insert Interface
+ * 
+ * @since 3.2.0
+ */
+interface InsertInterface extends DefaultInterface, VersioningInterface
+{
+
+/***[JCBGUI.power.main_class_code.134.$$$$]***/
+	/**
+	 * Insert rows to the database (with remapping and filtering columns option)
+	 *
+	 * @param   array    $data      Dataset to store in database [array of arrays (key => value)]
+	 * @param   string   $table     The table where the data is being added
+	 * @param   array    $columns   Data columns for remapping and filtering
+	 *
+	 * @return  bool
+	 * @since   3.2.0
+	 **/
+	public function rows(array $data, string $table, array $columns = []): bool;
+
+	/**
+	 * Insert items to the database (with remapping and filtering columns option)
+	 *
+	 * @param   array    $data         Data to store in database (array of objects)
+	 * @param   string   $table        The table where the data is being added
+	 * @param   array    $columns      Data columns for remapping and filtering
+	 *
+	 * @return  bool
+	 * @since   3.2.0
+	 **/
+	public function items(array $data, string $table, array $columns = []): bool;
+
+	/**
+	 * Insert row to the database
+	 *
+	 * @param   array    $data      Dataset to store in database (key => value)
+	 * @param   string   $table     The table where the data is being added
+	 *
+	 * @return  bool
+	 * @since   3.2.0
+	 **/
+	public function row(array $data, string $table): bool;
+
+	/**
+	 * Insert item to the database
+	 *
+	 * @param   object    $data     Dataset to store in database (key => value)
+	 * @param   string   $table     The table where the data is being added
+	 *
+	 * @return  bool
+	 * @since   3.2.0
+	 **/
+	public function item(object $data, string $table): bool;/***[/JCBGUI$$$$]***/
+
+}
+
---
+++ libraries/jcb_powers/JCB.Joomla/src/Jcbtest/Table.php
@@ -0,0 +1,75 @@
+<?php
+
+/***[JCBGUI.power.licensing_template.136.$$$$]***/
+/**
+ * @package    Joomla.Component.Builder
+ *
+ * @created    4th September, 2022
+ * @author     Llewellyn van der Merwe <https://dev.vdm.io>
+ * @git        Joomla Component Builder <https://git.vdm.dev/joomla/Component-Builder>
+ * @copyright  Copyright (C) 2015 Vast Development Method. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+/***[/JCBGUI$$$$]***/
+
+namespace JCB\Joomla\Jcbtest;
+
+
+use JCB\Joomla\Interfaces\TableInterface;
+use JCB\Joomla\Abstraction\BaseTable;
+
+
+/**
+ * Jcbtest Tables
+ * 
+ * @since 3.2.0
+ */
+class Table extends BaseTable implements TableInterface
+{
+
+/***[JCBGUI.power.main_class_code.136.$$$$]***/
+	/**
+	 * All areas/views/tables with their field details
+	 *
+	 * @var     array
+	 * @since 3.2.0
+	 **/
+	protected array $tables = [
+		'test' => [
+			'group' => [
+				'name' => 'group',
+				'label' => 'COM_JCBTEST_TEST_GROUP_LABEL',
+				'type' => 'text',
+				'title' => true,
+				'list' => 'tests',
+				'store' => NULL,
+				'tab_name' => 'Details',
+				'db' => [
+					'type' => 'VARCHAR(10)',
+					'default' => '',
+					'GUID' => '0d4a5caa-2199-4fb1-b07f-bd8071d71dbe',
+					'null_switch' => 'NULL',
+					'unique_key' => false,
+					'key' => true,
+				],
+				'link' => NULL,
+			],
+			'access' => [
+				'name' => 'access',
+				'label' => 'Access',
+				'type' => 'accesslevel',
+				'title' => false,
+				'store' => NULL,
+				'tab_name' => NULL,
+				'db' => [
+					'type' => 'INT(10) unsigned',
+					'default' => '0',
+					'key' => true,
+					'null_switch' => 'NULL',
+				],
+			],
+		],
+	];/***[/JCBGUI$$$$]***/
+
+}
+
